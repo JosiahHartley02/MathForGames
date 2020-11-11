6868104055f52d49134c0662e710b9c96bb2599a
JosiahAIE <josiah.t.hartley@gmail.com>
2020-11-11 15:51:11 -0600

    Finished changing 3d game game.cs

--- MathForGames3d/Game.cs
+++ MathForGames3d/Game.cs
@@ -17,6 +17,14 @@ namespace MathForGames3D
         {
             get { return _gameOver; } set { _gameOver = value; }
         }
+        public static bool GetKeyDown(int key)
+        {
+            return Raylib.IsKeyDown((KeyboardKey)key);
+        }
+        public static bool GetKeyPressed(int key)
+        {
+            return Raylib.IsKeyPressed((KeyboardKey)key);
+        }
         public static int CurrentSceneIndex
         {
             get
@@ -24,10 +32,7 @@ namespace MathForGames3D
                 return _currentSceneIndex;
             }
         }
-        public static Scene GetCurrentScene()
-        {
-            return _scenes[_currentSceneIndex];
-        }
+
         private void Start()
         {
             Raylib.InitWindow(1024, 760, "Math For Games");
@@ -37,6 +42,7 @@ namespace MathForGames3D
             _camera.up = new System.Numerics.Vector3(0.0f, 1.0f, 0.0f);
             _camera.fovy = 45.0f;
             _camera.type = CameraType.CAMERA_PERSPECTIVE;
+
         }
 
         private void Update()
@@ -72,6 +78,88 @@ namespace MathForGames3D
                 Draw();
             }
             End();
+        }
+        public static Scene GetCurrentScene()
+        {
+            return _scenes[_currentSceneIndex];
+        }
+        public static Scene GetScene(int index)
+        {
+            if (index < 0 || index >= _scenes.Length)
+                return new Scene();
+
+            return _scenes[index];
+        }
+        public static void SetCurrentScene(int index)
+        {
+            //If the index is not within the range of the the array return
+            if (index < 0 || index >= _scenes.Length)
+                return;
+
+            //Call end for the previous scene before changing to the new one
+            if (_scenes[_currentSceneIndex].Started)
+                _scenes[_currentSceneIndex].End();
+
+            //Update the current scene index
+            _currentSceneIndex = index;
+        }
+        public static int AddScene(Scene scene)
+        {
+            //If the scene is null then return before running any other logic
+            if (scene == null)
+                return -1;
+
+            //Create a new temporary array that one size larger than the original
+            Scene[] tempArray = new Scene[_scenes.Length + 1];
+
+            //Copy values from old array into new array
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                tempArray[i] = _scenes[i];
+            }
+
+            //Store the current index
+            int index = _scenes.Length;
+
+            //Sets the scene at the new index to be the scene passed in
+            tempArray[index] = scene;
+
+            //Set the old array to the tmeporary array
+            _scenes = tempArray;
+
+            return index;
+        }
+        public static bool RemoveScene(Scene scene)
+        {
+            //If the scene is null then return before running any other logic
+            if (scene == null)
+                return false;
+
+            bool sceneRemoved = false;
+
+            //Create a new temporary array that is one less than our original array
+            Scene[] tempArray = new Scene[_scenes.Length - 1];
+
+            //Copy all scenes except the scene we don't want into the new array
+            int j = 0;
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                if (tempArray[i] != scene)
+                {
+                    tempArray[j] = _scenes[i];
+                    j++;
+                }
+                else
+                {
+                    sceneRemoved = true;
+                }
+            }
+
+            //If the scene was successfully removed set the old array to be the new array
+            if (sceneRemoved)
+                _scenes = tempArray;
+
+            return sceneRemoved;
         }
     }
 }
